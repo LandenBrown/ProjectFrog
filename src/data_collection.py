da8bcaaee525fd9b610c782024a221be91346bf0
Landen Brown <landensbrown@gmail.com>
2020-12-12 16:12:50 -0500

    Simulations Started
    Mating, Starvation, old age death is now implemented

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,4 +1,4 @@
-import random
+import random, time
 
 from frog_species import Frog
 from world_biome import Biome
@@ -8,15 +8,15 @@ from predator_species import Predator
 
 #First names
 
-frog_first_names = ["Foro", "Litho", "Oro", "Morro", "Jerro", "Kiro"]
+frog_first_names = ["Foro", "Litho", "Oro", "Morro", "Jerro", "Kiro", "Lero", "Shira", "Geru", "Fortha", "Terelu", "Maxor"]
 predator_first_names = ["Veres", "Treves", "Meres", "Heres", "Nores", "Maxes"]
 
 #Middle names
-frog_middle_names = ["te", "de", "le", "vur", "lor", "lex", "ret", "vet", "cret", "feg", "van", "vax", "cet"]
+frog_middle_names = ["te", "de", "le", "vur", "lor", "lex", "ret", "vet", "cret", "feg", "van", "vax", "cet", "hat", "setti", "fetti", "cureti"]
 predator_middle_names = ["de", "ret", "rat", "raf", "fer", "dur", "lur", "bay", "tray", "tre", "jer"]
 
 #Last names
-frog_last_names = ["lithius", "birtius", "crethius", "ferelias", "trallium", "borallian"]
+frog_last_names = ["lithius", "birtius", "crethius", "ferelias", "trallium", "borallian", "sillian", "terillian", "victus", "victillian", "fortillia", "shillia", "camillia"]
 predator_last_names = ["relious", "celius", "tricillian", "gorian", "filthian", "vestian", "snichian"]
 
 def create_a_frog_name(fnames, mnames, lnames):
@@ -102,9 +102,24 @@ def create_an_initial_frog(int_atts, ext_atts):
     #Gerenate random starvation period
     randomStarvation = random.randint(min_food_days, max_lifespan_days)
 
+
+    #Generate mate chance per day
+    min_mate_chance = 1
+    max_mate_chance = 8
+    #Generate random mate chance 
+    randomMateChance = random.randint(min_mate_chance, max_mate_chance)
+
+    #Generate infertility
+    infertileChance = random.randint(1, 100)
+    fertility = True
+    if infertileChance >= 50:
+        fertility = True
+    else:
+        fertility = False
+
     #Creat new frog and return it
-    newFrog = Frog(randomName, RandomExtAtt, RandomIntAtt, 1, randomLifespan, 0, randomStarvation, False)
-    return newFrog
+    return Frog(randomName, RandomExtAtt, RandomIntAtt, 1, randomLifespan, 0, randomStarvation, False, randomMateChance, fertility)
+    
 
 #frog1 = create_an_initial_frog(frog_int_atts, frog_ext_atts)
 #print(frog1.name,frog1.internal_attributes,frog1.external_attributes,str(frog1.lifespan_days))
@@ -137,7 +152,7 @@ def create_an_initial_predator(int_atts, ext_atts):
     ###Calculate Lifespan
     #Set lifespan constraints
     min_lifespan_days = 20
-    max_lifespan_days = 100
+    max_lifespan_days = 200
     #Generate random lifespan
     randomLifespan = random.randint(min_lifespan_days, max_lifespan_days)
 
@@ -148,9 +163,7 @@ def create_an_initial_predator(int_atts, ext_atts):
 #predator1 = create_an_initial_predator(predator_int_atts, predator_ext_atts)
 #print(predator1.name,predator1.internal_attributes,predator1.external_attributes,str(predator1.lifespan_days))
 
-#Biomes
 
-standardJungle = Biome("Standard", "Tropical", "Rainforest", "Normal", "Picked", "Consistent", "Standard")
 
 
 #Here we must figure out how we are going to handle the giant list of frogs that is going to spawn
@@ -165,11 +178,100 @@ def update_lastFrogNumber():
     global lastFrogNumber
     lastFrogNumber += 1
     completeFrogName = newFrogName+str(lastFrogNumber)
-    print(completeFrogName)
-
-
-update_lastFrogNumber()
-update_lastFrogNumber()
-update_lastFrogNumber()
+    return completeFrogName
 
 ####Testing object creation
+
+
+# class FrogList:
+#     def __init__(self, title, frogs):
+#         self.title = title
+#         self.data = data
+#     def list_frogs(self):
+#         print("\n"+self.title)
+#         for k in self.data.keys():
+#             print("-"*self.data[k]+" "+k)
+
+# data = {"a":4, "b":7, "c":8}
+# initial_list = FrogList("Simple Frog List", data)
+
+
+
+
+###Dynamic frog creation steps
+
+#create a dictionary to store frogs in
+#Create a name for the key to reference each frog (update last frog number)
+#gather what the object attributes are going to be
+#Create the actual object
+#append it in the dictionary
+
+
+####Simple game testing
+
+#creating static dict for frogs
+frog_dict = {update_lastFrogNumber():create_an_initial_frog(frog_int_atts, frog_ext_atts)}
+frog_dict[update_lastFrogNumber()] = create_an_initial_frog(frog_int_atts, frog_ext_atts)
+#print(frog_dict["frog1"].name)
+
+#Biomes
+
+standardJungle = Biome("Standard", "Tropical", "Rainforest", "Normal", "Picked", "Consistent", "Standard")
+
+WorldAgeInDays = 0
+#Test list iteration as well as find food chance + death
+# all(x.is_dead!=True for x in frog_dict.values())
+x = 2
+while x != 1:
+    if x == 3:
+        print("No simulation.")
+        break
+    WorldAgeInDays+=1
+    print("It is now day ", str(WorldAgeInDays))
+    for frog in frog_dict.values():
+        #Let the frog look for food.
+        if frog.is_dead == False:
+            #Instead of find food, each frog needs to have a RunDailySimulation function that incorporates everything 
+            frog.findFood(standardJungle)
+            if frog.checkMating():
+                #Get list of frogs that are currently alive
+                frogsCurrentlyAlive = [v for k,v in frog_dict.items() if v.is_dead == False] 
+                #get length of that list minus 1 for list calculation   
+                numberOfFrogsCurrentlyAlive = (len(frogsCurrentlyAlive) - 1)
+                #pick a random frog in that list
+                randomAliveFrog = random.randint(0, numberOfFrogsCurrentlyAlive)
+                #Mate with this frog and add to dictionary
+                frog.Mate(frogsCurrentlyAlive[randomAliveFrog], create_an_initial_frog)
+                #how the fuck do i add this frog to the dictionary
+                frog_dict[update_lastFrogNumber()] = frog.Mate(frogsCurrentlyAlive[randomAliveFrog], create_an_initial_frog)
+                print("############################### A NEW FROG HAS BEEN BORN")
+                #time.sleep(2)
+                print(len(frog_dict))
+                #time.sleep(2)
+                break
+                
+
+
+    if WorldAgeInDays > 5000:
+        print("World age is 5000, ending.")
+        break
+    if all(x.is_dead==True for x in frog_dict.values()):
+        print ("the final frog has died.")
+        break
+    #if all(x.is_dead==True for x in frog_dict.values()):
+        #print("THE LAST FROG JUST DIED. EXITING")
+        #break
+
+print("All frogs died on day", str(WorldAgeInDays))
+print("A total of " + str(len(frog_dict)) + " frogs lived on your planet")
+for frog in frog_dict.values():
+    print(frog.name)
+
+
+#Testing grabbing the frogs that are currently alive
+#myList = [v.name for k,v in frog_dict.items() if v.is_dead == False]       
+#print (myList) 
+#works 
+
+
+    
--- src/frog_species.py
+++ src/frog_species.py
@@ -2,7 +2,7 @@ import random
 
 
 class Frog:
-    def __init__(self, name, external_attributes, internal_attributes, days_alive, lifespan_days, days_without_food, starvation_days, is_dead):
+    def __init__(self, name, external_attributes, internal_attributes, days_alive, lifespan_days, days_without_food, starvation_days, is_dead, mate_chance, infertile):
         self.name = name
         self.external_attributes = external_attributes
         self.internal_attributes = internal_attributes
@@ -11,24 +11,65 @@ class Frog:
         self.days_without_food = days_without_food
         self.starvation_days = starvation_days
         self.is_dead = is_dead
+        self.mate_chance = mate_chance
+        self.infertile = infertile
 
 
     def checkLifespan(self):
         #Check to see if the frog has died of old age yet. 
+        self.days_alive+=1
         if self.days_alive >= self.lifespan_days:
-            print("This frog is now dead.")
+            print("###################################   A FROG HAS DIED OF OLD AGE")
+            self.is_dead = True
             #####Kill frog
-        else:
-            print("This frog is still alive.")
+    
 
 
     def checkStarvation(self):
         #check to see if the frog has died of starvation.
         if self.days_without_food >= self.starvation_days:
-            print("This frog is now dead.")
+            print("###################################   A FROG HAS DIED OF STARVATION")
             ######Kill frog
+            self.is_dead = True
+
+    def checkMating(self):
+        #Check to see if the frog is fertile or not
+        if self.infertile == True:
+            #print("This frog is infertile")
+            pass
         else:
-            print("This frog has not starved yet.")
+            #generate random number between 1-100 for mate chance potential comparison
+            mateComparison = random.randint(1,100)
+            if self.mate_chance >= mateComparison:
+                #print("######################### A NEW FROG CAN MATE")
+                return True
+            else:
+                return False
+    
+    def Mate(self, x, initializeFrog):
+        #Grab an internal and external attribute from the other frog
+        new_int_att = x.internal_attributes[0]
+        new_ext_att = x.external_attributes[0]
+        #Grab an internal and external attribute from myself 
+        self_int_att = self.internal_attributes[0]
+        self_ext_att = self.external_attributes[0]
+
+        #Combine the lists into an interal_atts list and an eternal_atts list
+        internal_atts = []
+        internal_atts.append(new_int_att)
+        internal_atts.append(self_int_att)
+        external_atts = []
+        external_atts.append(new_ext_att)
+        external_atts.append(self_ext_att)
+
+        return initializeFrog(internal_atts, external_atts)
+
+
+
+        
+
+
+    
 
 
     def findFood(self, biome):
@@ -38,39 +79,39 @@ class Frog:
         foodRandom = random.randint(1, 100)
         #Is the biome food density low
         if biome.food_density == "Low":
-            #25% chance of finding food that day
-            if foodRandom >= 75: 
-                print("We have found food.")
+            #5% chance of finding food that day
+            if foodRandom >= 95: 
+                #print("We have found food.")
                 #Set days without food to 0
                 self.days_without_food = 0
             else: 
-                print("We have not found food today.")
+                #print("We have not found food today.")
                 #Set days without food to + 1
                 self.days_without_food += 1
                 #check starvation status
                 self.checkStarvation()
         #Is the biome food density standard
         if biome.food_density == "Standard":
-            #50% chance of finding food that day
-            if foodRandom >= 50: 
-                print("We have found food.")
+            #10% chance of finding food that day
+            if foodRandom >= 90: 
+                #print("We have found food.")
                 #Set days without food to 0
                 self.days_without_food = 0
             else: 
-                print("We have not found food today.")
+                #print("We have not found food today.")
                 #Set days without food to + 1
                 self.days_without_food += 1
                 #check starvation status
                 self.checkStarvation()
         #Is the biome food density high
         if biome.food_density == "High":
-            #50% chance of finding food that day
-            if foodRandom >= 75: 
-                print("We have found food.")
+            #15% chance of finding food that day
+            if foodRandom >= 85: 
+                #print("We have found food.")
                 #Set days without food to 0
                 self.days_without_food = 0
             else: 
-                print("We have not found food today.")
+                #print("We have not found food today.")
                 #Set days without food to + 1
                 self.days_without_food += 1
                 #check starvation status
